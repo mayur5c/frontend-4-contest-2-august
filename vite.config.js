1cf14513a0c6dcb91669d1ba06c884830ebf6f90
Mayur Chopra <mayur5c@live.in>
2023-08-19 16:14:29 +0530

    Updated base url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/f4-contest-3/",
+  base: "/frontend-4-contest-2-august/",
   plugins: [react()],
 });
